54b4f81a3ece8046fbe251de55e1f79fd4a833c6
redactedofficial <93684044+Sebix12@users.noreply.github.com>
2024-02-28 19:43:13 +0100

    Update omega.bat

--- omega.bat
+++ omega.bat
@@ -1,2 +1,46 @@
 @set debug=true
 @if "%debug%" == "false" echo off
+title OMEGA
+
+::vars
+set defloc=%~dp0
+set db=%defloc%db
+set kernel=%defloc%kernel.bat
+::end
+
+::check everything
+if not exist kernel.bat goto :get
+:contkern
+if not exist dbm.bat goto :getdbm
+:contdbm
+if not exist db goto :getldb
+:contldb
+::dont know what to check so ill put stuff later
+goto :start
+
+::code for check
+:getkernel
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Sebix12/OMEGA-II/main/kernel.bat', 'kernel.bat')"
+goto :contkern
+::------------
+:getdbm
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Sebix12/OMEGA-II/main/dbm.bat', 'dbm.bat')"
+goto :contdbm
+::------------
+:getldb
+if not exist db mkdir db
+cd %db%
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Sebix12/OMEGA-II/main/db.zip', 'db.zip')"
+cd %defloc%
+goto :contldb
+::------------
+
+::end : start
+:start
+
+:terminal
+set terminal=
+set /p terminal=">"
+goto :execcommand
+
+:execcommand

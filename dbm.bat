c53eb61a4511c8ecc614c44eaba7a1d57abcfc2f
redactedofficial <93684044+Sebix12@users.noreply.github.com>
2024-03-08 14:58:05 +0100

    Update dbm.bat

--- dbm.bat
+++ dbm.bat
@@ -8,18 +8,21 @@ if "%1" == "list" goto :list
 if "%1" == "run" goto :run
 if "%1" == "remove" goto :remove
 if "%1" == "get" goto :getpl
-
+echo not supported yet...
+goto :ext
 
 
 ::----------------crypt-----------------
 :encode
-certutil -encode %2.bat %2.db >> NUL
-del /q %2.bat
+if exist %2.bat certutil -encode %2.bat %2.db >> NUL
+if exist %2.bat del /q %2.bat
+if exist %2.db echo encoded successfully.
 goto :ext
 
 :decode
-certutil -decode %2.db %2.bat >> NUL
-del /q %2.db
+if exist %2.db certutil -encode %2.db %2.bat >> NUL
+if exist %2.db del /q %2.db
+if exist %2.bat echo decoded successfully.
 goto :ext
 ::----------------crypt-----------------
 
@@ -30,10 +33,13 @@ goto :ext
 cd %defloc%
 tar -cf db.zip %db%*
 cd %db%
+echo compiled
 goto :ext
 
 :decompile
 tar -xf db.zip -C %db%
+del db.zip
+echo compiled
 goto :ext
 
 ::-----------------
@@ -56,16 +62,16 @@ echo %2 does not exist try "help"
 goto :ext
 
 :remove
-if exist %2.bat del /q %2.bat
-if exist %2.db del /q %2.db
+if exist %2.bat del /q %2.bat && echo removed %2.bat
+if exist %2.db del /q %2.db && echo removed %2.db
 goto :ext
 
 :getpl
-echo downloading %2
 if exist %2.db echo %2 is already downloaded
 if exist %2.db goto :ext
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Sebix12/OMEGA-II-plugin-repo/main/%2.db', '%2.db')"
-if exist %2.db echo downloaded
+echo downloading %2
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Sebix12/OMEGA-II-plugin-repo/main/%2.db', '%2.db')" > NUL
+if exist %2.db echo download succeeded
 if not exist %2.db echo download failed
 goto :ext
 

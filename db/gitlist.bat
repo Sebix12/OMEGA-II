587ea6804826e822fa3dd921d032022dc3d01902
redactedofficial <93684044+Sebix12@users.noreply.github.com>
2024-03-08 15:24:25 +0100

    Update gitlist.bat

--- db/gitlist.bat
+++ db/gitlist.bat
@@ -1,3 +1,15 @@
+@echo off
+echo Install DataBase Files?
+set /p dep=y/n$
+if "%dep%" == "n" goto :ext
+if "%dep%" == "y" echo installing
+::start
 set github=https://raw.githubusercontent.com/Sebix12/OMEGA-II-plugin-repo/main/
+::vars end
+
+::files
 powershell -Command "(New-Object Net.WebClient).DownloadFile('%github%editor.db', 'editor.db')"
 if exist editor.db echo downloaded tool - editor
+
+::exit
+:ext
